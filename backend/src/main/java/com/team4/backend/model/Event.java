3ffc3be6af948a22512bf5be3446be1f0e3087fd
eeb2988 <eeb2988@g.rit.edu>
2024-06-26 16:09:31 -0400

    created correct DB link and created restful springboot commands

--- backend/src/main/java/com/team4/backend/model/Event.java
+++ backend/src/main/java/com/team4/backend/model/Event.java
@@ -1,5 +1,38 @@
 package com.team4.backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "posts", schema = "public")
+@Builder
 public class Event {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "employee_name")
+    private String employeeName;
+
+//    @Column(name = "date_of_event")
+//    private LocalDateTime dateOfEvent;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "employee_id")
+    private String employeeId;
+
+    @Column(name = "event_name")
+    private String eventName;
+
+//    @Column(name = "created_at")
+//    private LocalDateTime createdAt;
 }
